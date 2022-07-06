35a35535a1b2a24d26869c2df78119cf77da41bd
AlucarD <skoriy1290@gmail.com>
2022-07-06 14:23:30 +0300

    refactoring App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,14 @@
 import style from './app.module.css';
 import About from '../About/About';
+import Contacts from "../Contacts/Contacts";
+import Projects from "../Projects/Projects";
 
 const App = () => {
   return(
     <div className={style.app}>
       <About />
+      <Contacts />
+      <Projects />
     </div>
   )
 }
