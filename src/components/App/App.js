9c4817c8ec4492d16abaeda11b0d7e57d2d86e52
AlucarD <skoriy1290@gmail.com>
2022-07-06 16:44:02 +0300

    add MyStack component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import style from './app.module.css';
 import About from '../About/About';
 import Contacts from '../Contacts/Contacts';
 import Projects from '../Projects/Projects';
+import MyStack from '../MyStack/MyStack';
 
 const App = () => {
   return(
@@ -10,6 +11,7 @@ const App = () => {
       <div className={style.wrapper}>
         <About />
         <Contacts />
+        <MyStack />
         <Projects />
       </div>
       <div className={style.footer}>Буду рад сотрудничеству !</div>
---
+++ src/components/MyStack/MyStack.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './stack.module.css';
+
+const MyStack = () => {
+  return(
+    <section className={style.stack}>
+      <h2 className={style.title}>Я использую:</h2>
+      <div className={style.stackList}>
+        <img src={'https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw'} alt={'HTML'}/>
+        <img src={'https://play-lh.googleusercontent.com/RTAZb9E639F4JBcuBRTPEk9_92I-kaKgBMw4LFxTGhdCQeqWukXh74rTngbQpBVGxqo'} alt={'CSS / SCSS'}/>
+        <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png'} alt={'JavaScript'}/>
+        <img src={'https://miro.medium.com/0*iib7UI_uS5HOu6Fz.png'} alt={'TypeScript'}/>
+        <img src={'https://ensocore.com/media/61/reactjs-logo-sticker%20%281%29.jpg'} alt={'React'}/>
+        <img src={'https://miro.medium.com/max/800/1*ZxmcaOzul181U6BT5z-Ipw.png'} alt={'Redux'}/>
+      </div>
+    </section>
+  );
+};
+
+export default MyStack;
