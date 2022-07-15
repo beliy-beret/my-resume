605bb99fed9319a9e84fc3526b46d10fe8727b3a
AlucarD <skoriy1290@gmail.com>
2022-07-15 17:15:30 +0300

    add adaptive style for About component

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -5,7 +5,11 @@ import style from './projectCard.module.css';
 const ProjectCard = ({title, screenShot, url}) => {
   return(
     <div className={style.card}>
-      <a href={url} target={'_blank'} rel="noreferrer"><img src={screenShot} alt={'Screen project'} /></a>
+      <div className={style.imgContainer}>
+        <a href={url} target={'_blank'} rel="noreferrer">
+          <img src={screenShot} alt={'Screen project'} />
+        </a>
+      </div>
       <h2 className={style.title}><a href={url} target={'_blank'} rel="noreferrer">{title}</a></h2>
     </div>
   );
